edc91a82eef4e5afca237ce69220e749b44cfd1b
parthiviR <parthivi.rastogi@gmail.com>
2023-07-09 21:07:40 -0400

    NEW LINE

--- js/index.js
+++ js/index.js
@@ -41,37 +41,35 @@ function scrollToPrimaryNav() {
     let scrollToDiv = document.getElementById("primary-header");
     let isScrolling = false;
     let originalTop = scrollToDiv.offsetTop;
-  
-    sourceDiv.addEventListener("mousewheel", function(event) {
-      // Prevent default scrolling behavior
-      event.preventDefault();
-  
-      // Check if the source div has been scrolled vertically
-      if (sourceDiv && !isScrolling) {
-        isScrolling = true;
-  
-        // Scroll the target div into view after a delay
-        setTimeout(function() {
-          if (window.scrollY > originalTop) {
-            scrollToDiv.style.position = "fixed";
-            scrollToDiv.style.top = "0";
-          } else {
-            scrollToDiv.style.position = "static";
-          }
-  
-          // Scroll the target div into view
-          scrollToDiv.scrollIntoView({
-            behavior: "smooth", // Add a smooth scroll transition
-            block: "start" // Scroll to the top of the target div
-          });
-  
-          // Reset isScrolling flag after the scroll animation completes
-          setTimeout(function() {
-            isScrolling = false;
-          }, 1000); // Add a delay before resetting the flag (adjust the duration as needed)
-        }, 500); // Add a delay before scrolling (adjust the duration as needed)
-      }
-    });
-  }
-  
 
+    sourceDiv.addEventListener("mousewheel", function (event) {
+        // Prevent default scrolling behavior
+        event.preventDefault();
+
+        // Check if the source div has been scrolled vertically
+        if (sourceDiv && !isScrolling) {
+            isScrolling = true;
+
+            // Scroll the target div into view after a delay
+            setTimeout(function () {
+                if (window.scrollY > originalTop) {
+                    scrollToDiv.style.position = "fixed";
+                    scrollToDiv.style.top = "0";
+                } else {
+                    scrollToDiv.style.position = "static";
+                }
+
+                // Scroll the target div into view
+                scrollToDiv.scrollIntoView({
+                    behavior: "smooth", // Add a smooth scroll transition
+                    block: "start" // Scroll to the top of the target div
+                });
+
+                // Reset isScrolling flag after the scroll animation completes
+                setTimeout(function () {
+                    isScrolling = false;
+                }, 1000); // Add a delay before resetting the flag (adjust the duration as needed)
+            }, 500); // Add a delay before scrolling (adjust the duration as needed)
+        }
+    });
+}
